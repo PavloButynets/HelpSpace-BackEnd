427eecd358bebcd1496f0028d3a9d2ea6c0e1e9d
ktr <pavlobutynez@gmail.com>
2025-02-15 23:56:43 +0200

    added validation in registerDTO

--- src/application/dto/request/RegisterUserDTO.ts
+++ src/application/dto/request/RegisterUserDTO.ts
@@ -1,3 +1,6 @@
+import {lengths, regex} from "../../../consts/validation";
+import {IsEmail, IsString, Matches, MaxLength, MinLength} from "class-validator";
+
 export enum UserRole {
     ADMIN = "admin",
     USER = "user",
@@ -5,9 +8,32 @@ export enum UserRole {
 }
 
 export class RegisterUserDTO {
+
+    @IsString()
+    @MinLength(lengths.MIN_NAME_LENGTH)
+    @MaxLength(lengths.MAX_NAME_LENGTH)
+    @Matches(regex.NAME_PATTERN, {
+        message: 'First name must contain only letters.'
+    })
     firstName: string;
+
+    @IsString()
+    @MinLength(lengths.MIN_NAME_LENGTH)
+    @MaxLength(lengths.MAX_NAME_LENGTH)
+    @Matches(regex.NAME_PATTERN, {
+        message: 'Last name must contain only letters.'
+    })
     lastName: string;
+
+    @IsString()
+    @IsEmail()
     email: string;
+
+    @IsString()
+    @MinLength(lengths.MIN_PASSWORD_LENGTH)
+    @Matches(regex.PASSWORD_PATTERN, {
+        message: 'Password must contain at least one digit, one lowercase letter, and no spaces.',
+    })
     password: string;
 
     constructor(
--- src/presentation/routes/auth.ts
+++ src/presentation/routes/auth.ts
@@ -4,8 +4,8 @@ import {AUTH_TYPES} from "../../container/types/AuthTypes";
 import {AuthController} from "../controllers/AuthController";
 import {asyncWrapper} from "../middlewares/asyncWrapper";
 import {validationMiddleware} from "../middlewares/validationMiddleware";
-import {registerValidationEntity} from "../../validation/schemas/register";
 import {LoginRequestDTO} from "../../application/dto/request/LoginRequest";
+import {RegisterUserDTO} from "../../application/dto/request/RegisterUserDTO";
 
 
 
@@ -15,7 +15,7 @@ export const authRoutes = () => {
     const router = express.Router();
     const authController = container.get<AuthController>(AUTH_TYPES.AuthController);
     router.post('/register',
-        validationMiddleware(registerValidationEntity),
+        validationMiddleware(RegisterUserDTO),
         asyncWrapper(authController.register.bind(authController)));
     router.post('/login',
         validationMiddleware(LoginRequestDTO),
